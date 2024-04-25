f3375ed7b3cf1c187ccda0ce3161bf5946f77597
hyenee99 <twoddal2@naver.com>
2024-04-25 23:13:18 +0900

    구명보트

---
+++ programmers/P42885.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.*;
+
+public class P42885 {
+	public int solution(int[] people, int limit) {
+		int answer = 0;
+		Arrays.sort(people);
+		int index = 0;
+
+		for (int i = people.length - 1; i >= index; i--) {
+			if (people[i] + people[index] <= limit) {
+				index++;
+				answer++;
+			} else {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
