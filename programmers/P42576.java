cfa50d5e8e04cbbdb044eff971898e0d388cff90
hyenee99 <90517148+hyenee99@users.noreply.github.com>
2024-06-05 17:20:24 +0900

    완주하지 못한 선수

---
+++ programmers/P42576.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        String answer="";
+        
+        for(int i=0;i<completion.length;i++){
+            if(!completion[i].equals(participant[i])){
+                answer+=participant[i];
+                break;
+            }
+        }
+        
+        if(answer.equals(""))
+            answer+=participant[participant.length-1];
+        
+        
+        return answer;
+    }
+}
