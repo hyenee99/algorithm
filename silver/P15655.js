1b71682d2dd4ca9640183e846f5efdef6be429c3
hyenee99 <90517148+hyenee99@users.noreply.github.com>
2025-06-07 23:52:26 +0900

    N과 M(6)

---
+++ silver/P15655.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let [n, m] = input[0].split(" ").map(Number);
+let numbers = input[1].split(" ").map(Number);
+numbers.sort((a, b) => a - b);
+let result=[];
+
+function dfs(depth,start,arr){
+  if(depth==m){
+    result.push(arr.join(" "));
+    return;
+  }
+
+  for(let i=start;i<n;i++){
+    arr.push(numbers[i]);
+    dfs(depth+1,i+1,arr);
+    arr.pop();
+  }
+}
+
+dfs(0,0,[]);
+console.log(result.join("\n"));
