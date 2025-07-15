d9767a59d5fad029d825e8727d6a487dd946699a
hyenee99 <90517148+hyenee99@users.noreply.github.com>
2025-07-15 17:15:28 +0900

    좌표 압축

---
+++ P18870.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let arr = input[1].split(" ").map(Number);
+let set = new Set(arr);
+let sorted_arr = [...set];
+sorted_arr.sort((a,b)=>a-b); 
+
+let map = new Map();
+for(let i=0;i<sorted_arr.length;i++){
+  map.set(sorted_arr[i],i);
+}
+
+const answer = arr.map(num => map.get(num));
+console.log(answer.join(" "));
